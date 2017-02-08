1006a0e84563d56fcee8dcdb3142102c3b114019
Joachim Nilsson <troglobit@gmail.com>
2017-02-08 22:47:28 +0100

    First example of a db consumer, reads last entry
    Signed-off-by: Joachim Nilsson <troglobit@gmail.com>

---
+++ web.c
@@ -0,0 +1,18 @@
+#include "common.h"
+
+int main(int argc, char *argv[])
+{
+	struct wdb entry;
+
+	openlog(NULL, LOG_PID, LOG_DAEMON);
+
+	if (db_get(&entry))
+		return 1;
+
+
+	printf("%s: temp %.1f °C, pressure %.1f hPa, humidity %.1f %%RH, light %.1f lux\n",
+	       entry.timestamp, entry.temperature, entry.pressure, entry.humidity,
+	       entry.illuminance - entry.infrared);
+
+	return 0;
+}
