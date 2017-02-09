4045367523a4ddf6022a3ac12766d4021d206829
Joachim Nilsson <troglobit@gmail.com>
2017-02-09 22:54:29 +0100

    Minor whitespace
    Signed-off-by: Joachim Nilsson <troglobit@gmail.com>

--- web.c
+++ web.c
@@ -9,7 +9,6 @@ int main(int argc, char *argv[])
 	if (db_get(&entry))
 		return 1;
 
-
 	printf("%s: temp %.1f °C, pressure %.1f hPa, humidity %.1f %%RH, light %.1f lux\n",
 	       entry.timestamp, entry.temperature, entry.pressure, entry.humidity,
 	       entry.illuminance - entry.infrared);
