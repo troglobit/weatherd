61ba9983a4749874f5bd3233c2d43ffa85d4b5e0
Joachim Nilsson <troglobit@gmail.com>
2017-02-09 22:54:39 +0100

    Add more includes
    Signed-off-by: Joachim Nilsson <troglobit@gmail.com>

--- common.h
+++ common.h
@@ -2,6 +2,9 @@
 #include <stdlib.h>
 #include <string.h>
 #include <syslog.h>
+#include <unistd.h>
+
 #include "db.h"
+#include "sensor.h"
 
 size_t strlcpy(char *dst, const char *src, size_t dsize);
